e3a53e0870ca7b0edc3c29e29332f8dd90b1bc37
faiztoufiq <138994716+faiztoufiq@users.noreply.github.com>
2023-08-07 00:08:53 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+          <!---Swipper css link-->
+          <link  rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <!---css link-->
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+<!---header section start-->
+<section class="header">
+    <a href="Home.php" class="logo">Travel</a>
+
+<nav class="navlbar">
+    <a href="home.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="packages.php">Packages</a>
+    <a href="book.php">Book</a>
+    </nav>
+<div id="menu-btn" class="fas fa-bars"></div>
+</section>
+<!---home section-->
+<section class="home">
+
+<div class="swiper home-slider">
+
+<div class="swiper-wrapper">
+
+<div class="swiper-slide slide" style="background: url('https://img.freepik.com/free-photo/travel-concept-with-landmarks_23-2149153256.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais')no-repeat; ">
+<div class="contnet">
+   <span>Explore, Discovery, Travel</span> 
+   <h3>Travel Around the world</h3>
+   <a href="packages.php" class="btn">Discover More</a>
+    </div>
+    
+    </div>
+
+   <div class="swiper-slide slide" style="background: url('https://img.freepik.com/premium-photo/commercial-port-montevideo-rio-de-la-plata_545448-5660.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.1.1421741829.1680935883&semt=ais')no-repeat;">
+   <div class="contnet">
+   <span>Explore, Discovery, Travel</span> 
+   <h3>Discover the New Places</h3>
+   <a href="packages.php" class="btn">Discover More</a>
+    </div>
+    </div>
+
+    <div class="swiper-slide slide" style="background: url('https://img.freepik.com/free-photo/booking-hotel-reservation-travel-destination-concept_53876-121094.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.1.1421741829.1680935883&semt=ais')no-repeat; ">
+   <div class="contnet">
+   <span>Explore, Discovery, Travel</span> 
+   <h3>Make your Tour Worth While</h3>
+   <a href="packages.php" class="btn">Discover More</a>
+    </div>
+    </div>
+
+
+
+</div>
+<div class="swiper-button-next"></div>
+<div class="swiper-button-prev"></div>
+
+</div>
+
+</section>
+
+
+<!---service section-->
+<section class ="services">
+<h1 class="heading-title">Our Services</h1>
+<div class="box-container">
+
+<div class="box">
+<img src="https://img.freepik.com/free-vector/vintage-mountain-lettering_79603-2268.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+<h3>Adventure<h3>
+</div>
+
+<div class="box">
+<img src="https://img.freepik.com/free-vector/flat-vintage-travel-logo_23-2148177882.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+<h3>Tour Guide<h3>
+</div>
+
+<div class="box">
+<img src="https://img.freepik.com/premium-vector/trekking-logo_565520-397.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+<h3>trekking<h3>
+</div>
+
+<div class="box">
+<img src="https://img.freepik.com/premium-vector/camping-outdoor-circle-emblem-vector-illustration-isolated_289688-349.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+<h3>Camp fire<h3>
+</div>
+
+<div class="box">
+<img src="https://img.freepik.com/free-vector/coffee-transportation-jeep_24877-54440.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+<h3>Off Road<h3>
+</div>
+
+<div class="box">
+<img src="https://img.freepik.com/premium-vector/camp-element-vector-icon-design-illustration-template_1948-1533.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+<h3>Camping<h3>
+</div>
+
+</div>
+</section>
+<!----homeabout section--->
+<section class="home-about">
+
+<div class="image">
+    <img src="https://img.freepik.com/free-photo/friends-sitting-choosing-destination_23-2147654152.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.1.1421741829.1680935883&semt=ais" alt="">
+</div>
+
+<div class="content">
+    <h3>About Us</h3>
+    <p>faiz toufiq. Tech Services Inc. provides workforce management solutions.
+            The Company offers strategic staffing, background screening, drug testing, 
+            prequalification, tax and benefit administration,<br> and talent solutions.
+            faiz toufiq. Tech Services provides clinical, commercial, educational, 
+            and financial staffing services.</p>
+<a href="about.php" class="btn">Read More</a>
+        </div>
+</section>
+
+<!--package section--->
+
+<section class="home-pakage">
+
+<h1 class="heading-title">Our Packages<h1>
+    <div class="box-container">
+
+<div class ="box">
+<div class="image">
+<img src="https://img.freepik.com/free-photo/tall-mosque-architecture_1308-41475.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.1.1421741829.1680935883&semt=ais" alt="">
+</div>
+<div class="content">
+<h3>Adventure & Tour</h3>
+<p></p>
+<a href="book.php" class="btn">Book Now</a>
+</div>
+</div>
+
+<div class ="box">
+<div class="image">
+<img src="https://img.freepik.com/free-photo/mosque-is-lit-up-night-with-lights-reflecting-water_1340-23825.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+</div>
+<div class="content">
+<h3>Adventure & Tour</h3>
+<p></p>
+<a href="book.php" class="btn">Book Now</a>
+</div>
+</div>
+
+<div class ="box">
+<div class="image">
+<img src="https://img.freepik.com/premium-photo/putra-mosque-putrajaya-malaysia_491078-916.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.1.1421741829.1680935883&semt=ais" alt="">
+</div>
+<div class="content">
+<h3>Adventure & Tour</h3>
+
+<p></p>
+<a href="book.php" class="btn">Book Now</a>
+</div>
+</div>
+</div>
+    <div class="load-more"><a href="packages.php" class="btn">Load More</packages></a></div>
+</section>
+
+
+
+<!--home offer section--->
+<section class="home-offer">
+    <div class="content">
+<h3>UPTO 70% OFF</h3>
+<p>The Company offers strategic staffing, background screening, drug testing, 
+            prequalification, tax and benefit administration,<br> and talent solutions.
+            faiz toufiq. Tech Services provides clinical, commercial, educational, 
+            and financial staffing services</p>
+<a href="book.php" class="btn">Book Now</a>
+</div>
+</section>
+
+
+<!--footer link--->
+<section class="footer">
+<div class="box-container">
+<div class="box">
+    <h3>Quick Links</h3>
+    <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>About</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>Package</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>Book</a>
+</div>
+<div class="box">
+    <h3>Extra Links</h3>
+    <a href="#"><i class="fas fa-angle-right"></i>Ask Questions</a>
+    <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+    <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+    <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+</div>
+<div class="box">
+    <h3>Contact info</h3>
+    <a href="#"><i class="fas fa-phone"></i>03204067710</a>
+    <a href="#"><i class="fas fa-phone"></i>03238839130</a>
+    <a href="#"><i class="fas fa-envelope"></i>faiztoufiq123@gmail.com</a>
+    <a href="#"><i class="fas fa-map"></i>New shalimar colony multan road Lahore -042</a>
+</div>
+<div class="box">
+    <h3>follow us</h3>
+    <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+    <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+    <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+    <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+</div>
+</div>
+<div class="credit">Created by | <span>faiz toufiq Tech Services</span> | all rights reserved..!</div>
+</section>   
+<!---Swipper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<!---js link-->
+<script src="script.js"></script>
+</body>
