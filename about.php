128285ba9a47a5d3890af916997d8582e6c6d859
faiztoufiq <138994716+faiztoufiq@users.noreply.github.com>
2023-08-07 00:10:43 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About</title>
+          <!---Swipper css link-->
+          <link  rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <!---css link-->
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+<!---header section start-->
+<section class="header">
+    <a href="Home.php" class="logo">Travel</a>
+
+<nav class="navlbar">
+    <a href="home.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="packages.php">Packages</a>
+    <a href="book.php">Book</a>
+    </nav>
+<div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+<div class="heading"style="background-image: url('https://img.freepik.com/free-psd/editable-travel-banner-template-psd-bloggers_53876-140101.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais'); no-repeat">
+<h1>About Us</h1>
+</div>
+<!--about section--->
+<section class="about">
+    <div class="image">
+        <img src="https://img.freepik.com/free-psd/editable-travel-banner-template-psd-bloggers_53876-140101.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+    </div>
+    <div class="content">
+        <h3>Why Choose Us?</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem praesentium eveniet excepturi, et nihil architecto, dolor sed alias ut deleniti obcaecati perspiciatis cupiditate voluptatem possimus deserunt, minima doloremque provident assumenda?
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, perspiciatis velit provident incidunt iure expedita cum ipsum quidem quos suscipit eligendi, veritatis voluptatum! Possimus et, repellat ratione autem ullam voluptatum.</p>
+    <div class="icons-container">
+        <div class="icon">
+            <i class="fas fa-map"></i>
+            <span>Top Destinations</span>
+        </div>
+        <div class="icon">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>Affordable Price</span>
+        </div>
+        <div class="icon">
+            <i class="fas fa-headset"></i>
+            <span>24/7 Guide Services</span>
+        </div>
+
+    </div>
+    </div>
+</section>
+<!--review section--->
+<section class="reviews">
+
+<div class="swiper reviews-slider">
+
+<div class="swiper-wrapper">
+
+<div class="swiper-slider slide">
+    <div class="starts">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus recusandae hic cumque maxime. Blanditiis similique incidunt earum aliquid quibusdam eum et minus, neque odit enim hic. Voluptatum, beatae porro!</p>
+<h3>faiz toufiq</h3>
+<span>Traveler</span>
+<img src="https://img.freepik.com/premium-photo/beautiful-smiling-young-male-teacher-front-blurry-school-background_111797-2220.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+</div>
+
+<div class="swiper-slider slide">
+    <div class="starts">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus recusandae hic cumque maxime. Blanditiis similique incidunt earum aliquid quibusdam eum et minus, neque odit enim hic. Voluptatum, beatae porro!</p>
+<h3>Hamid gulzar</h3>
+<span>Traveler</span>
+<img src="https://img.freepik.com/free-photo/studio-shot-serious-young-unshaven-caucasian-male-entrepreneur-staring-camera-with-deep-thoughts-look-thinking-some-business-problem-people-modern-lifestyle-job-career-concept_343059-958.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+</div>
+
+<div class="swiper-slider slide">
+    <div class="starts">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus recusandae hic cumque maxime. Blanditiis similique incidunt earum aliquid quibusdam eum et minus, neque odit enim hic. Voluptatum, beatae porro!</p>
+<h3>Zain mirza</h3>
+<span>Traveler</span>
+<img src="https://img.freepik.com/free-photo/indoor-portrait-pleased-handsome-ordinary-european-man-with-moustache-beard-smiling-broadly-feeling-confident-happy-while-making-arrangement-wedding-gray-wall_176420-22384.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+</div>
+
+
+<div class="slide">
+    <div class="starts">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus recusandae hic cumque maxime. Blanditiis similique incidunt earum aliquid quibusdam eum et minus, neque odit enim hic. Voluptatum, beatae porro!</p>
+<h3>Anum</h3>
+<span>Traveler</span>
+<img src="https://img.freepik.com/free-photo/portrait-young-beautiful-woman-standing-smiling_114579-67121.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+</div>
+
+
+<div class="slide">
+    <div class="starts">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus recusandae hic cumque maxime. Blanditiis similique incidunt earum aliquid quibusdam eum et minus, neque odit enim hic. Voluptatum, beatae porro!</p>
+<h3>Amna</h3>
+<span>Traveler</span>
+<img src="https://img.freepik.com/free-photo/close-up-stylish-caucasian-girl-denim-jacket-going-spring-walk-looking-casual-camera-standing-against-white-background_176420-45053.jpg?size=626&ext=jpg&uid=R98829628&ga=GA1.2.1421741829.1680935883&semt=ais" alt="">
+</div>
+
+
+
+</div>
+
+</div>
+
+</section>
+
+
+<!--footer link--->
+<section class="footer">
+<div class="box-container">
+<div class="box">
+    <h3>Quick Links</h3>
+    <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>About</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>Package</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>Book</a>
+</div>
+<div class="box">
+    <h3>Extra Links</h3>
+    <a href="#"><i class="fas fa-angle-right"></i>Ask Questions</a>
+    <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+    <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+    <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+</div>
+<div class="box">
+    <h3>Contact info</h3>
+    <a href="#"><i class="fas fa-phone"></i>03204067710</a>
+    <a href="#"><i class="fas fa-phone"></i>03238839130</a>
+    <a href="#"><i class="fas fa-envelope"></i>faiztoufiq123@gmail.com</a>
+    <a href="#"><i class="fas fa-map"></i>New shalimar colony multan road Lahore -042</a>
+</div>
+<div class="box">
+    <h3>follow us</h3>
+    <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+    <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+    <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+    <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+</div>
+</div>
+<div class="credit">Created by | <span>faiz toufiq Tech Services</span> | all rights reserved..!</div>
+</section>   
+<!---Swipper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<!---js link-->
+<script src="script.js"></script>
+</body>
